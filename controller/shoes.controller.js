e816dd20abdea0fde667f930b3a8d809bf97f140
AntCannon <anthonycannonier@gmail.com>
2024-07-21 10:42:39 -0400

    Add getShoe
    1. Add query
    2. Add route

--- controller/shoes.controller.js
+++ controller/shoes.controller.js
@@ -3,9 +3,13 @@ const express = require("express");
 const shoes = express.Router();
 
 // queries
-const { getAllShoes } = require("../queries/shoes.queries.js");
+const { getAllShoes, getShoe } = require("../queries/shoes.queries.js");
+
+// middleware
+// validation
 
 // routes
+// index
 shoes.get("/", async (req, res) => {
   try {
     const allShoes = await getAllShoes();
@@ -14,5 +18,17 @@ shoes.get("/", async (req, res) => {
 		  res.status(404).json({error:"Resource not found"});
 	}
 });
+
+// show
+shoes.get('/:id', async (req, res) => {
+  const { id } = req.params
+  try {
+    const oneShoe = await getShoe(id)
+    res.json(oneShoe)
+  } catch(error) {
+      console.log(error)
+      res.status(404).json({ error: `Shoe with id ${id} not found.`})
+  } 
+})
 
 module.exports = shoes;
