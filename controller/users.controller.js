969df39d7a79f6ac18b41e1d255be17330e84584
antonio <antonio.fr.martinezc@hotmail.com>
2024-07-21 11:12:29 -0400

    implemented route controler and query to update user

--- controller/users.controller.js
+++ controller/users.controller.js
@@ -3,7 +3,11 @@ const express = require("express");
 const users = express.Router();
 
 // queries
-const { getAllUsers, getUser } = require("../queries/users.queries.js");
+const {
+	getAllUsers,
+	getUser,
+	updateUser,
+} = require("../queries/users.queries.js");
 
 // routes
 users.get("/", async (req, res) => {
@@ -14,15 +18,30 @@ users.get("/", async (req, res) => {
 		res.status(404).json({ error: "Resource not found" });
 	}
 });
+//create
 
+//show
 users.get("/:id", async (req, res) => {
 	try {
 		const { id } = req.params;
 		const user = await getUser(id);
 		res.status(200).json(user);
 	} catch (error) {
-		res.status(404).json({ error: "User not found" });
+		res.status(404).json({ msg: "User not found", error });
 	}
 });
+
+//update
+users.put("/:id", async (req, res) => {
+	try {
+		const { id } = req.params;
+		const updatedUser = await updateUser(id, req.body);
+		res.status(200).json(updatedUser);
+	} catch (error) {
+		res.status(200).json({ msg: "User not found", error });
+	}
+});
+
+//delete
 
 module.exports = users;
