b21e31eb399d6a9fba2ecf50ba6255f5f663390d
AntCannon <anthonycannonier@gmail.com>
2024-07-21 00:23:43 -0400

    Add users index routing
    1. Update shoes table to have random seller ids
    2. Add users route, controller, and queries
    3. Add getAllUsers

--- app.js
+++ app.js
@@ -1,21 +1,29 @@
+// dependencies
 const express = require("express");
 const app = express();
-
 const cors = require("cors");
+
+// controllers
+const shoesController = require("./controller/shoes.controller");
+
+// middleware
 app.use(cors());
 app.use(express.json());
 
-
-const shoesController = require("./controller/shoes.controller");
 app.use("/shoes", shoesController);
 
+const usersController = require("./controller/users.controller");
+app.use("/users", usersController);
 
-
+// healthcheck
 app.get("/", (req, res) => {
 	res.send("Welcome to Nikedas");
 });
 
+// 404
 app.get("*", (req, res) => {
 	res.status(404).json({ error: "Page not found" });
 });
+
+// export
 module.exports = app;
--- controller/shoes.controller.js
+++ controller/shoes.controller.js
@@ -1,12 +1,17 @@
+// dependencies
 const express = require("express");
 const shoes = express.Router();
-const { getShoes } = require("../queries/shoes.queries");
+
+// queries
+const { getAllShoes } = require("../queries/shoes.queries.js");
+
+// routes
 shoes.get("/", async (req, res) => {
-	try {
-		const shoes = await getShoes();
-		res.status(200).json(shoes);
-	} catch (error) {
-		res.status(404).json({error:"Resource not found"});
+  try {
+    const allShoes = await getAllShoes();
+		res.status(200).json(allShoes);
+	} catch(error) {
+		  res.status(404).json({error:"Resource not found"});
 	}
 });
 
