e8ee03c3b2a79349f242057425e37586c1633b3e
antonio <antonio.fr.martinezc@hotmail.com>
2024-07-21 10:49:07 -0400

    implemented route controler and query to fetch user by id

--- controller/users.controller.js
+++ controller/users.controller.js
@@ -3,16 +3,26 @@ const express = require("express");
 const users = express.Router();
 
 // queries
-const { getAllUsers } = require("../queries/users.queries.js");
+const { getAllUsers, getUser } = require("../queries/users.queries.js");
 
 // routes
 users.get("/", async (req, res) => {
-  try {
-    const allUsers = await getAllUsers();
-    res.status(200).json(allUsers);
-  } catch(error) {
-      res.status(404).json({error: "Resource not found"});
-  }
+	try {
+		const allUsers = await getAllUsers();
+		res.status(200).json(allUsers);
+	} catch (error) {
+		res.status(404).json({ error: "Resource not found" });
+	}
+});
+
+users.get("/:id", async (req, res) => {
+	try {
+		const { id } = req.params;
+		const user = await getUser(id);
+		res.status(200).json(user);
+	} catch (error) {
+		res.status(404).json({ error: "User not found" });
+	}
 });
 
 module.exports = users;
--- queries/users.queries.js
+++ queries/users.queries.js
@@ -2,14 +2,23 @@
 const db = require("../db/dbConfig");
 
 async function getAllUsers() {
-  try {
-    const allUsers = await db.any("SELECT * FROM users");
-    return allUsers;
-  } catch(error) {
-      throw error;
-  }
-};
+	try {
+		const allUsers = await db.any("SELECT * FROM users");
+		return allUsers;
+	} catch (error) {
+		throw error;
+	}
+}
+async function getUser(id) {
+	try {
+		const user = await db.one("SELECT * FROM users WHERE id = $1", id);
+		return user;
+	} catch (error) {
+		throw error;
+	}
+}
 
 module.exports = {
-  getAllUsers
-}
+  getAllUsers,
+  getUser,
+};
