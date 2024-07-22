a6c407d521fed3ba988fc66d421f6cf7b11ca6a3
AntCannon <anthonycannonier@gmail.com>
2024-07-22 19:00:34 -0400

    Setup db for deployment
    1. Create and populate prod_schema.sql
    2. Create and populate prod_seed.sql
    3. Update dbConfig.js

--- db/dbConfig.js
+++ db/dbConfig.js
@@ -1,25 +1,32 @@
 const pgp = require("pg-promise")();
 require("dotenv").config();
 
-const cn = {
-	host: process.env.PG_HOST,
-	port: process.env.PG_PORT,
-	database: process.env.PG_DATABASE,
-	user: process.env.PG_USER
-};
+const { DATABASE_URL, PG_HOST, PG_PORT, PG_DATABASE, PG_USER, PG_PASSWORD } =
+  process.env;
+const cn = DATABASE_URL
+  ? {
+      connectionString: DATABASE_URL,
+      max: 30,
+    }
+  : {
+      host: PG_HOST,
+      port: PG_PORT,
+      database: PG_DATABASE,
+      user: PG_USER,
+    };
 
 const db = pgp(cn);
 
 db.connect()
-	.then((cn) => {
-		const { user, host, port, database } = cn.client;
-		console.log(
-			"\x1b[90m" +
-				`Postgres connection established with user:${user}, host:${host}, port:${port}, database:${database}` +
-				"\x1b[0m"
-		);
-		cn.done();
-	})
-	.catch((error) => console.log("database connection error", error));
+  .then((cn) => {
+    const { user, host, port, database } = cn.client;
+    console.log(
+      "\x1b[90m" +
+        `Postgres connection established with user:${user}, host:${host}, port:${port}, database:${database}` +
+        "\x1b[0m"
+    );
+    cn.done();
+  })
+  .catch((error) => console.log("database connection error", error));
 
 module.exports = db;
