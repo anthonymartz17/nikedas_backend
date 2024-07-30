9bb62cf06988e898b39d0b5e909c20c9038c0823
antonio <antonio.fr.martinezc@hotmail.com>
2024-07-29 20:49:43 -0400

    updated production schema and seed

--- db/prod_schema.sql
+++ db/prod_schema.sql
@@ -1,19 +1,18 @@
-DROP DATABASE IF EXISTS nikedas_prod;
-CREATE DATABASE nikedas_prod;
+DROP DATABASE IF EXISTS nikedas_db;
+CREATE DATABASE nikedas_db;
 
-\c nikedas_prod;
+\c nikedas_db;
 
-CREATE TABLE
-  users
-  (
+CREATE TABLE users (
     id SERIAL PRIMARY KEY,
+    uid VARCHAR(255) UNIQUE NOT NULL,
     first_name VARCHAR(100),
     last_name VARCHAR(100),
-    email VARCHAR(255),
+    email VARCHAR(255) NOT NULL UNIQUE,
     phone_number VARCHAR(25),
     about_store TEXT,
     is_active BOOLEAN
-  );
+);
 
 CREATE TABLE shoes (
     id SERIAL PRIMARY KEY,
@@ -32,14 +31,12 @@ CREATE TABLE shoes (
     created_at TIMESTAMP,
     updated_at TIMESTAMP,
     deleted_at TIMESTAMP,
-    seller_id INT REFERENCES users(id)
+    seller_id VARCHAR(255) REFERENCES users(uid)
 );
 
--- CREATE TABLE
---   favorites
---   (
---     id SERIAL,
---     user_shoe_favorite TEXT PRIMARY KEY,
---     user_id INT REFERENCES users(id),
---     shoe_id INT REFERENCES shoes(id)
---   );
+CREATE TABLE favorites (
+    id SERIAL PRIMARY KEY,
+    user_id INT REFERENCES users(id),
+    shoe_id INT REFERENCES shoes(id),
+    UNIQUE(user_id, shoe_id)
+);
